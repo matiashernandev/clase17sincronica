28f5ea27bed4c87b40cac20da2322d22ac823ec8
nkwaaaa <nkwaaaa@gmail.com>
2022-09-08 20:48:05 -0300

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "https://nkwaaaa.github.io/clase17sincronica/",
     plugins: [react()],
-    base: "/clase17sincronica/",
 });
