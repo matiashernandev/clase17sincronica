a6fba28593254900cab5439b51418b6ff08b333f
nkwaaaa <nkwaaaa@gmail.com>
2022-09-08 23:08:22 -0300

    add helmet.png

--- src/pages/NewGame.jsx
+++ src/pages/NewGame.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import helmet from "../assets/images/helmet.png";
 
 function NewGame() {
     return (
         <div>
+            <img height="200px" src={helmet} alt={"optimizar no no?"} />
             <h3>¿Género?</h3>
             <div className="buttons">
                 <Link to="/clase17sincronica/character/hombre">
